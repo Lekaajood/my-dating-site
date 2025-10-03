e326b338db13122b3da6bb6ff3b187b19b31e3ea
Lekaajood <lassal.lekaa@gmail.com>
2025-10-03 05:45:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-async function loadFeed(){
-  const res = await fetch('people.json');
-  const items = await res.json();
-  const feed = document.getElementById('feed');
-  feed.innerHTML = items.map(renderCard).join('');
-}
-function renderCard(p){
-  return `<div class="card">
-    <img src="${p.photoURL}" alt="${p.name}">
-    <h3>${p.name}, ${p.age}</h3>
-    <p>${p.bio}</p>
-  </div>`;
-}
-window.onload = loadFeed;
+
